d14af71f400ffba5942ddb795f208dd0d1204873
Robert Rosborg <rosborg.robert@gmail.com>
2024-10-24 03:34:29 +0200

    Add Shuffle array extension

--- Assets/Scripts/ArrayExtensions.cs
+++ Assets/Scripts/ArrayExtensions.cs
@@ -14,4 +14,20 @@ public static class ArrayExtensions
         }
     }
 
+    /// <summary>
+    /// Performs a Fisher–Yates shuffle on an array.
+    /// </summary>
+    /// <param name="array">The array to be shuffled.</param>
+    /// <typeparam name="T">The type of the objects the array contains.</typeparam>
+    /// <seealso href="https://en.wikipedia.org/wiki/Fisher%E2%80%93Yates_shuffle">Fisher–Yates shuffle on Wikipedia</seealso>
+    public static void Shuffle<T>(this T[] array)
+    {
+        Random random = new Random();
+        int n = array.Length;
+        for (int i = 0; i < n; i++)
+        {
+            int j = random.Next(i, n);
+            (array[i], array[j]) = (array[j], array[i]);
+        }
+    }
 }
