e5c778619e400700be126b7a2f7eeef7847691ed
Robert Rosborg <rosborg.robert@gmail.com>
2024-10-24 23:16:36 +0200

    Add ToPrettyString function

--- Assets/Scripts/ArrayExtensions.cs
+++ Assets/Scripts/ArrayExtensions.cs
@@ -30,4 +30,32 @@ public static class ArrayExtensions
             (array[i], array[j]) = (array[j], array[i]);
         }
     }
+
+    /// <summary>
+    /// Produces a pretty <see cref="string"/> representation of an array, using brackets and commas.
+    /// </summary>
+    /// <param name="array">The array to produce a string from.</param>
+    /// <typeparam name="T">The type of the objects the array contains.</typeparam>
+    /// <returns>A pretty string representation of the array.</returns>
+    /// <example><c>new int[] { 1, 2, 3 }.ToPrettyString()</c> => <c>[1, 2, 3]</c></example>
+    public static string ToPrettyString<T>(this T[] array)
+    {
+        switch (array.Length)
+        {
+            case < 1:
+                return "[]";
+            case 1:
+                return $"[{array[0]}]";
+        }
+
+        string acc = "[";
+
+        for (int i = 0; i < array.Length - 1; i++)
+        {
+            acc += $"{array[i]}, ";
+        }
+        acc += $"{array[^1]}]";
+
+        return acc;
+    }
 }
