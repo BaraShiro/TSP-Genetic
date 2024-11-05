16bf21bf5b75f6028234214fb86a8c50de5080d1
Robert Rosborg <rosborg.robert@gmail.com>
2024-11-05 10:57:55 +0100

    Add word list with ~3000 english words

---
+++ Assets/Scripts/WordList.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+public static partial class WordList
+{
+    private const int NumberOfWords = 2957;
+    public static string GetRandomWord()
+    {
+        Random random = new Random();
+        int randomNumber = random.Next(NumberOfWords);
+        return Words[randomNumber];
+    }
+}
