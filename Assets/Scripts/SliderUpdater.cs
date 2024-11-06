41533d0fa2f81426646800e230dae932833d40b7
Robert Rosborg <rosborg.robert@gmail.com>
2024-11-06 10:04:09 +0100

    Updated slider

--- Assets/Scripts/SliderUpdater.cs
+++ Assets/Scripts/SliderUpdater.cs
@@ -8,15 +8,24 @@ public class SliderUpdater : MonoBehaviour
     private Slider slider;
 
     [SerializeField] private TMP_Text valueText;
+    [SerializeField] private bool inPercent;
 
     private void Awake()
     {
         slider = GetComponent<Slider>();
-        valueText.text = slider.value.ToString();
+        SetValueText();
     }
 
     public void SetValueText()
     {
-        valueText.text = slider.value.ToString();
+        if (slider.wholeNumbers)
+        {
+            valueText.text = $"{slider.value}{(inPercent ? "%": "")}";
+        }
+        else
+        {
+            valueText.text = $"{slider.value:0.00}{(inPercent ? "%": "")}";
+        }
+
     }
 }
